e1f45fff9aff5be56f17319ca96ebffc5ae75f69
SAkshi Kale <66623701+SAkshi2430@users.noreply.github.com>
2021-06-13 20:18:31 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+list1=[12,-7,5,64,-14]
+print("list1-",list1)
+print("output-",end=" ")
+for i in list1:
+    if(i>0):
+        str(i)
+        print(i ,end=" "+",")
+    
+print(" ")
+list2=[12,14,-95,3]
+print("list2-",list2)
+Output=[]
+for i in list2:
+    if(i>0):
+        Output.append(i)
+print("output-", end="")
+print(Output)
+    
+    
+    
